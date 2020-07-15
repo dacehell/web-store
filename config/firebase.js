27a74b94c188720df8c1b80f31b55e7dc2d54d6a
David <caldera.david78@gmail.com>
2020-07-15 17:47:19 -0400

    navbar

---
+++ config/firebase.js
@@ -0,0 +1,10 @@
+export default {
+  apiKey: "AIzaSyAEyJmXw1-cn-42zlQ0kVVAmBg-Au9r31M",
+    authDomain: "webstore-6d6ca.firebaseapp.com",
+    databaseURL: "https://webstore-6d6ca.firebaseio.com",
+    projectId: "webstore-6d6ca",
+    storageBucket: "webstore-6d6ca.appspot.com",
+    messagingSenderId: "655780873418",
+    appId: "1:655780873418:web:b24a7cbcbbde9ab8ada033"
+
+};
